b7953261bfe2b3e957d70c1ef4741da61b8e332e
Mari <40849185+marichiiin@users.noreply.github.com>
2023-10-24 15:33:09 +0800

    section-4
    This commit is all about swiper JS

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "4",
+  freeMode: true,
+  grabCursor: true,
+    
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+ 
+    });
