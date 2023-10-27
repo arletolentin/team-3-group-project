38504c3159f157782b5d6ebc50790482b6b476aa
marichiiin <40849185+marichiiin@users.noreply.github.com>
2023-10-27 06:48:40 +0000

    Deploying to gh-pages from @ arletolentin/team-3-group-project@a4722b9923bf9e43730b7a18516088f5126b80f4 🚀

--- index.63d11348.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{slidesPerView:"4",freeMode:!0,grabCursor:!0,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.63d11348.js.map
---
+++ index.a5d96123.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:10,grabCursor:!0,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{640:{slidesPerView:3,spaceBetween:20},1024:{slidesPerView:4,spaceBetween:20}}});
+//# sourceMappingURL=index.a5d96123.js.map
