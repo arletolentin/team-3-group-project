a4722b9923bf9e43730b7a18516088f5126b80f4
Mari <40849185+marichiiin@users.noreply.github.com>
2023-10-27 14:48:08 +0800

    responsiveness

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: "4",
-  freeMode: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  //freeMode: true,
   grabCursor: true,
-    
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // 640: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 50,
+    // },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
  
-    });
+});
